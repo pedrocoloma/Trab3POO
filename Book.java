231ea6c2c2c24a9bcc613e437f638c96944e1beb
jvguima <jvguima@yahoo.com.br>
2015-05-30 18:43:59 -0300

    Update Book.java
    >Datas de retorno e aluguel mudadas para Strings
    >novas bookID e renterID, chaves primarias para melhor manipular os objetos
    >Getters e Setters Criados
    >Segundo construtor para inicializar referência a partir de dados de Arquivo

--- Book.java
+++ Book.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabalho3;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.*;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,13 +16,15 @@ public class Book {
         
     protected  String title;
     protected  String author;
+    protected  String type;
+    protected  int renterID;//MANTEM COMO USER? USER ID?
     protected  int year;
+    protected  int bookID;
     
+    protected String rentDate;
+    protected String returnDate;
     
-    protected GregorianCalendar rentDate;
-    protected GregorianCalendar returnDate;
-    
-
+    // GETTERS
     public String getTitle(){
         return this.title;
     }
@@ -35,41 +34,135 @@ public class Book {
     public int getYear(){
         return this.year;
     }
-
-    public GregorianCalendar getRentDate(){
+    public int getBookID(){
+        return this.bookID;
+    }
+    public int getRenterID(){
+        return this.renterID;
+    }
+    public String getType(){
+        return this.type;
+    }    
+    public String getRentDate(){
         return this.rentDate;
     }
-    
-    public GregorianCalendar getReturnDate(){
+    public String getReturnDate(){
         return this.returnDate;
     }
-
+    
+    //SETTERS
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    public void setBookID(int id){
+        this.bookID = id;
+    }
+    public void setRenterID(int id){
+        this.renterID = id;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setRentDate(String date){
+        this.rentDate = date;
+    }
+    public void setReturnDate(String date){
+        this.returnDate = date;
+    }
+    
     void writeFile(){
-
-        try( FileWriter writer = new FileWriter("books.csv", true)) //Append books.csv
+        // Le um inteiro do arquivo numberOfBooks.txt
+        Scanner scanner = null;
+        int x = 0;
+        try {
+            scanner = new Scanner(new FileReader("numberOfBooks.txt"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GeneralBook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+          String livros = scanner.next();
+          x = Integer.parseInt(livros);
+           
+        // Incrementa e salva no mesmo arquivo
+        x++;
+        this.bookID = x;
+        //System.out.println(x);
+        PrintWriter escritor = null;
+        try {
+            escritor = new PrintWriter("numberOfBooks.txt", "UTF-8");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GeneralBook.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(GeneralBook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        escritor.println(x);
+        escritor.close();
+        scanner.close();
+        
+        try(FileWriter writer = new FileWriter("books.csv", true)) //Append books.csv
         {
-           writer.write(""+this.title);
-           writer.write(",");
-           writer.write(""+this.author);
-           writer.write(",");           
-           writer.write(""+this.year);
-           writer.write(",");           
-           writer.write(""+this.rentDate);           
-           writer.write(",");           
-           writer.write(""+this.returnDate); 
-           /*writer.write(",");           
-           if(this.renter != null){
-                writer.write(""+this.renter.getName()); 
+           
+           writer.write(""+this.bookID+",");
+           writer.write(""+this.type+",");
+           writer.write(""+this.title+",");
+           writer.write(""+this.author+",");
+           writer.write(""+this.year+",");
+           writer.write(""+this.rentDate+",");           
+           writer.write(""+this.returnDate+","); 
+           if(this.renterID != -1){
+                writer.write(""+this.renterID); 
+                //writer.close();
            }
            else{
-                writer.write("null"); 
-           }*/
-           writer.write("\t");           
+                writer.write("-1"); 
+                //;writer.close();
+           }
+           writer.write("\n");           
         } catch (IOException ex) {
             Logger.getLogger(Trabalho3.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+        
+        
+    // construtor  
+    Book(){
+        Scanner s = new Scanner (System.in);
+
+        System.out.println("Enter the Title: ");
+        this.title = s.nextLine();
+            
+        System.out.println("Enter the Author: ");
+        this.author = s.nextLine();
+        
+        System.out.println("Enter the Year: ");
+        this.year = s.nextInt();
+        
+        this.type = ("book");
+        
+        this.renterID = -1;
+        this.rentDate = "00/00/0000";
+        this.returnDate = "00/00/0000";
+
+        //this.writeFile();
+    }
+        
+    Book(int bookID, String type, String title, String author, int year, String rentDate, String returnDate, int renterID){
+        
+        this.bookID = bookID;
+        this.type = type;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        
+        this.rentDate = rentDate;
+        this.returnDate = returnDate;
+        this.renterID = renterID;
+    }
 
     /*
     Book(){
