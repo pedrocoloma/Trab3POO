b0d4acfa24b5b690782506141026093ebf4fdf4c
jvguima <jvguima@yahoo.com.br>
2015-05-30 19:08:24 -0300

    Update CommonUser.java

--- CommonUser.java
+++ CommonUser.java
@@ -5,10 +5,30 @@
  */
 package trabalho3;
 
+
 /**
  *
  * @author User
  */
 public class CommonUser extends User{
     
+    private final int limit; //Limite de quantos livros podem ser alguados
+    
+
+    CommonUser(){
+        super();
+        this.type = ("Common");
+        this.limit = 2;
+        //this.writeFile(); //Construtor para a primeira inserção em arquivo, ativar append
+    }
+    
+    
+    CommonUser(int userID, String type, String name, int numberOfRentedBooks, boolean active){
+        this.userID = userID;
+        this.name = name;
+        this.type = type;
+        this.limit = 2;        
+        this.numberOfRentedBooks = numberOfRentedBooks;
+        this.active = active;
+    }
 }
