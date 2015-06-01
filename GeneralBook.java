7b45c93f481fb7e7f10e321a041b3cdfb599737b
jvguima <jvguima@yahoo.com.br>
2015-06-01 20:02:39 -0300

    Update GeneralBook.java
    Correção da setagem das datas de retorno e aluguel

--- GeneralBook.java
+++ GeneralBook.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabalho3;
 
 
-/**
- *
- * @author User
- */
 public class GeneralBook extends Book {
             
 
@@ -17,8 +9,6 @@ public class GeneralBook extends Book {
         super();
         this.type = ("General");
         this.renterID = -1;
-        this.rentDate = null;
-        this.returnDate = null;
         
 
         this.writeFile();
